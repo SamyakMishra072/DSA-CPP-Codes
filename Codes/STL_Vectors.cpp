fab06f24a5a054b5ae2565bcf4fa57bba10c1eb6
SamyakMishra072 <edithstandsfor@gmail.com>
2025-01-28 22:12:12 +0530

    Created STL VECTORS

--- Codes/STL_Vectors.cpp
+++ Codes/STL_Vectors.cpp
@@ -6,8 +6,10 @@ int main() {
     vector <int> v; //initiliaze a vector.
     v.push_back(1); //adding "1" to it.
     v.emplace_back(2);
-    vector <pair<int,int>>vec;
-    
+    vector <pair<int,int>>vec; //vec=vector of a pair.
+    vec.push_back({1,2}); //it needs curly braces.
+    vec.emplace_back(100,200); //no extra curly braces are needed.
+    vector <int> v(400,121); //this is also allowed.
 
 
 
