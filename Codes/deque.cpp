7841089ca263b88d334f2e6da842dde242ca4015
SamyakMishra072 <edithstandsfor@gmail.com>
2025-01-29 00:38:24 +0530

    DEQUE IN STL

--- Codes/deque.cpp
+++ Codes/deque.cpp
@@ -3,17 +3,14 @@ using namespace std;
 
 int main() {
     deque<int>dq; //initialized a deque in stl in c++.
-    
+    dq.push_back(1);
+    /* emplace_back, 
+        push_front,emplace_front , pop_back;
+        pop_front;
+        dq.back();  //Accesses the last element of the deque.
+        dq.front();   //dq.front(): Accesses the first element of the deque.
 
-
-
-
-
-
-
-
-
-
-
+//rest all are similar just like vector in stl.
+*/
     return 0;
 }
