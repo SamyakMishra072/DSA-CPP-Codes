00fc7bf893d50787b8d9ee1242817ae097e9e225
SamyakMishra072 <edithstandsfor@gmail.com>
2025-01-28 21:55:14 +0530

    STL_pairs

---
+++ Codes/STL_Vectors.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    //we use vectors when speicific container size is now known beforehand.
+    vector <int> v; //initiliaze a vector.
+    v.push_back(1); //adding "1" to it.
+    v.emplace_back(2);
+    vector <pair<int,int>>vec;
+    
+
+
+
+
+
+    return 0;
+}
