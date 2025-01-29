723988f2e5710f3129c768e202e744bd02448694
SamyakMishra072 <edithstandsfor@gmail.com>
2025-01-29 16:18:51 +0530

    Sets

--- Codes/set.cpp
+++ Codes/set.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 int main() {
-    
+    set<int>st;
+    st.insert(0);
+    st.emplace(8);
+    st.insert(4);
+    st.insert(1);
+    st.insert(1); //it won't store 1 again as Set Only Stores Unique and
+    //Ordered Elements. 
+    st.insert(12);
+    st.insert(9);
+    auto it=st.find(3);
+    st.erase(12); // takes logarithmic amount of time.
+
 
 
 
