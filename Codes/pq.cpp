aadb447081ac626a7f8063afaf522ecf5005658f
SamyakMishra072 <edithstandsfor@gmail.com>
2025-01-29 15:37:27 +0530

    PQ

--- Codes/pq.cpp
+++ Codes/pq.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main() {
     priority_queue<int>pq;
-    pq.push(1);
-    pq.push(5);
-    pq.push(4);
-    pq.emplace(9);
-    cout<<pq.top()<<endl;
+    pq.push(1); // 1
+    pq.push(5); // 1 5
+    pq.push(4); // 5 4 1
+    pq.emplace(0); // 5 4 1 0
+    cout<<pq.top()<<endl; // top() function prints the largest element.
     //SWAP function as normally.
     
 
