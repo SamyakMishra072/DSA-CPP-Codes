643da33ce74d62990ec6d26c1f505fd3cdc88892
SamyakMishra072 <edithstandsfor@gmail.com>
2025-01-29 14:52:28 +0530

    priority queue added

--- Codes/pq.cpp
+++ Codes/pq.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main() {
+    priority_queue<int>pq;
+    pq.push(1);
+    pq.push(5);
+    pq.push(4);
+    pq.emplace(9);
+    cout<<pq.top()<<endl;
+    //SWAP function as normally.
     
 
 
