cde7aaa88d7467a1bed98e023afafa240f155b1f
SamyakMishra072 <edithstandsfor@gmail.com>
2025-01-29 14:48:01 +0530

    Added Queue.cpp

--- Codes/queue.cpp
+++ Codes/queue.cpp
@@ -2,25 +2,18 @@
 using namespace std;
 
 int main() {
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    queue<int>q;
+    q.push(1); // 1
+    q.push(2); //1 2
+    q.emplace(6); // 1 2 6
+    q.back() +=5; // 1 2 11
+    cout<<q.back()<<endl; // 11
+    q.pop(); // 1 gets removed.
+    //q.pop(); // 2 gets popped out
+    //q.pop(); 11 gets removed.
+    cout<<q.back()<<endl; //1
+    //cout<<q.front()<<endl; 
+    q.swap(q); //no change for this case.
+    cout<<q.size()<<endl; // 2
     return 0;
 }
