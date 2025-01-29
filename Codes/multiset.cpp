3c7a1163f73ffbe0d8c9ae3e04f064c02026f194
SamyakMishra072 <edithstandsfor@gmail.com>
2025-01-29 19:07:39 +0530

    MultiSet Added

--- Codes/multiset.cpp
+++ Codes/multiset.cpp
@@ -3,13 +3,16 @@ using namespace std;
 
 int main() {
     multiset<int>ms;
+    //multiset is a sorted set .
     //same as set but can allso store duplicate elements.
     ms.insert(1);
     ms.insert(8);
     ms.insert(2);
     ms.insert(8);
-    ms.erase(1); //all 1's erased.
+    ms.erase(1); //all 1's erased. //2,8,8
     int count=ms.count(8); // count the no of occurences of 8 and finally store it in count variable.
-    ms.erase(ms.find(2));
+    //count =2.
+    ms.erase(ms.find(2)); //ms.erase(iterator) removes only one occurrence of 2.
+    // 8,8
     return 0;
 }
