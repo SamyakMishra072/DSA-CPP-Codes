b665cd0f2382d29b0fc1d304ab84110859e435d8
SamyakMishra072 <edithstandsfor@gmail.com>
2025-01-29 19:02:58 +0530

    MultiSet Added

--- Codes/multiset.cpp
+++ Codes/multiset.cpp
@@ -3,23 +3,13 @@ using namespace std;
 
 int main() {
     multiset<int>ms;
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //same as set but can allso store duplicate elements.
+    ms.insert(1);
+    ms.insert(8);
+    ms.insert(2);
+    ms.insert(8);
+    ms.erase(1); //all 1's erased.
+    int count=ms.count(8); // count the no of occurences of 8 and finally store it in count variable.
+    ms.erase(ms.find(2));
     return 0;
 }
