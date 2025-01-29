b5a426be962108986aa5ddb5730ee96a05ade3ad
SamyakMishra072 <edithstandsfor@gmail.com>
2025-01-29 19:11:29 +0530

    Unordered Set

---
+++ Codes/unorderedset.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    unordered_set<int>st;
+    //EVERYTHING IS SAME AS AN SET,ONLY DIFFERENCE IS THAT
+    //IT IS NOT SORTED.
+    //lower_bound and upper_bound() does not work here.
+
+    return 0;
+}
