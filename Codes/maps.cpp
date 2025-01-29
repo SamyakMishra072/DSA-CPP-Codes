c9045b61e0d94b6b4ba1c27613c7555e7f37aa97
SamyakMishra072 <edithstandsfor@gmail.com>
2025-01-29 19:43:39 +0530

    Maps.cpp create succesfully

---
+++ Codes/maps.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Map with integer keys and integer values
+    map<int, int> m1;
+
+    // Insert key-value pair into m1
+    m1[1] = 2;  // m1 = { {1, 2} }
+    
+    // Insert key-value pair using emplace
+    m1.emplace(3, 1);  // m1 = { {1, 2}, {3, 1} }
+    
+    // Corrected insert statement
+    m1.insert({2, 4}); // m1 = { {1, 2}, {2, 4}, {3, 1} }
+
+    // Print the map contents
+    for (auto it : m1) {
+        cout << it.first << "---> " << it.second <<" "<<endl;
+    }
+
+    // Accessing elements
+    cout << m1[1] << endl; // Outputs 2
+    cout << m1[5] << endl; // Outputs 0 (default-initialized since key 5 does not exist)
+
+    return 0;
+}
