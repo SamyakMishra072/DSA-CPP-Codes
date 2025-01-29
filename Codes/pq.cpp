0778cafa877a26a12ddbc6e69e197c24090eec2f
SamyakMishra072 <edithstandsfor@gmail.com>
2025-01-29 16:11:44 +0530

    PQ

--- Codes/pq.cpp
+++ Codes/pq.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+    //MAXIMUM HEAP
     priority_queue<int>pq;
     pq.push(1); // 1
     pq.push(5); // 1 5
@@ -9,12 +10,11 @@ int main() {
     pq.emplace(0); // 5 4 1 0
     cout<<pq.top()<<endl; // top() function prints the largest element.
     //SWAP function as normally.
-    
-
-
-
-
-
+    //MINIMUM HEAP
+    priority_queue<int,vector<int>,greater<int>>pqr;
+    pqr.push(7); //7
+    pqr.push(2); //2 7
+    cout<<pqr.top()<<endl; //2 gets printed.f
 
     return 0;
 }
