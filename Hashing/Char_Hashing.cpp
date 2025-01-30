42eb9eaafa0ab62325357be45c51bda4dfa67c6f
SamyakMishra072 <edithstandsfor@gmail.com>
2025-01-30 23:17:08 +0530

    Added Character Hashing

--- Hashing/Char_Hashing.cpp
+++ Hashing/Char_Hashing.cpp
@@ -2,12 +2,22 @@
 using namespace std;
 
 int main() {
-    
-
-
-
-
-
-
+    string s;
+    cout<<"Enter the String \n";
+    cin>>s;
+    //Pre Computation
+    int hash[26]={0};
+    for (int i=0;i<s.size();i++) {
+        hash[s[i]-'a']++;
+    }
+    cout<<"Enter the Number of Queries"<<endl;
+    int q;
+    cin>>q;
+    while (q--) {
+        char c;
+        cout<<"Enter the Character \n";
+        cin>>c;
+        cout<<hash[c-'a']<<endl;
+    }
     return 0;
 }
