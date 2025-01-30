ff07347a676c881e3eea795598b4f8869f310b97
SamyakMishra072 <edithstandsfor@gmail.com>
2025-01-30 12:52:44 +0530

    Hashing Q1 Solved

--- Hashing/Introduction.cpp
+++ Hashing/Introduction.cpp
@@ -11,33 +11,25 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-
     // PRE-COMPUTATION
-    int hash[13] = {0}; // Assuming elements are in the range [0, 12]
+    int hash[13] = {0}; 
     for (int i = 0; i < n; i++) {
         hash[arr[i]] += 1;
     }
-
-    cout << "\nPreprocessing done! You can now query frequencies.\n";
-    
-    // Query input
     int q;
     cout << "Enter the number of queries: ";
     cin >> q;
 
-    cout << "\nEnter " << q << " numbers to check their frequency:\n";
+    cout << "\nEnter number to check their frequency:\n";
     while (q--) {
         int number;
         cin >> number;
-        
         // FETCHING
         if (number >= 0 && number <= 12) {
-            cout << "Frequency of " << number << " = " << hash[number] << endl;
+            cout << "Frequency is "<< hash[number] << endl;
         } else {
-            cout << "Number " << number << " is out of range (0-12)!\n";
+            cout << "Number out of range (0-12)!\n";
         }
     }
-
-    cout << "Program execution completed!\n";
     return 0;
 }
