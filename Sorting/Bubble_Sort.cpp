b98237ff2affb79b451fbb36741c0883a2e1b85b
SamyakMishra072 <edithstandsfor@gmail.com>
2025-02-13 23:53:50 +0530

    Added Sorting Algorithms

--- Sorting/Bubble_Sort.cpp
+++ Sorting/Bubble_Sort.cpp
@@ -2,12 +2,16 @@
 using namespace std;
 void bubble_sort(int arr[],int n) {
     for (int i=n-1;i>=0;i--) {
+        int flag=0;
         for (int j=0;j<=i-1;j++) {
             if (arr[j]>arr[j+1]) {
                 int temp=arr[j+1];
                 arr[j+1]=arr[j];
                 arr[j]=temp;
             }
+        }
+        if (flag==0) {
+            break;
         }
     }
 }
